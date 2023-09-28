b871649201c081b515c73272d83dfffb4d85f327
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-09-28 13:04:39 +0530

    created useRestaurantList custom hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,27 +4,14 @@ import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import image from '../utils/image.jpg';
+import useRestaurantList from "../utils/useRestaurantList";
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
-    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
     const [searchText, setSearchText] = useState("");
 
     console.log("Body Rendered");
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-
-        const json = await data.json();
-        console.log(json);
-        // setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
+    const { listOfRestaurants, filteredRestaurant, setFilteredRestaurant } = useRestaurantList();
 
     const onlineStatus = useOnlineStatus();
     if (onlineStatus === false) {
